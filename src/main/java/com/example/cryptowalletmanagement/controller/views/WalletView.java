237a3bbb26c06185b1c6e529421a4425f62b8413
sluzzledude <sluzzledude@gmail.com>
2025-02-23 20:14:30 +0000

    refact: add and refactor Dockerfiles and build configurations for the application

--- src/main/java/com/example/cryptowalletmanagement/controller/views/WalletView.java
+++ src/main/java/com/example/cryptowalletmanagement/controller/views/WalletView.java
@@ -2,6 +2,11 @@ package com.example.cryptowalletmanagement.controller.views;
 
 import com.example.cryptowalletmanagement.dto.wallet.WalletDTO;
 
+/**
+ * a record that represents a simplified view of an wallet
+ * @param email
+ * @param token
+ */
 public record WalletView(String email, String token) {
     public static WalletView fromWalletDTO(WalletDTO walletDTO) {
         return new WalletView(walletDTO.email(), walletDTO.token());
